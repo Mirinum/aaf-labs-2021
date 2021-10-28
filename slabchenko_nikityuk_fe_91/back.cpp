8d6c99763beec3c2cd64092054fb298449bf7d6f
Mirinum <edien.pierce.wd@gmail.com>
2021-10-28 04:51:47 +0300

    Added some things just to do some things

--- slabchenko_nikityuk_fe_91/back.cpp
+++ slabchenko_nikityuk_fe_91/back.cpp
@@ -2,63 +2,250 @@
 #include "outputtools.hpp"
 #include <climits>
 
-#define BRANCHSIZE 30
-
 using namespace std;
+
+node* VectorList::push_back(vector<string> d){
+	if(root==nullptr){
+		root = new node(this);
+		root->data = d;
+		s++;
+		return root;
+	}
+	else{
+		node* temp = root;
+		node* temp_prev = nullptr;
+		while(temp->next != nullptr){
+			temp_prev = temp;
+			temp = temp->next;
+		}
+		temp->next = new node(this);
+		temp->next->data = d;
+		temp->prev = temp_prev;
+		s++;
+		return temp->next;
+	}
+}
+
+node::node(VectorList* p){
+	parent = p;
+}
+
+node& VectorList::operator[](int index){
+	node* temp = root;
+	while(index != 0){
+		index--;
+		if(temp->next == nullptr) throw DBexception("Invalid index!");
+		temp = temp->next;
+	}
+	return *temp;
+}
+
+int VectorList::size(){
+	return s;
+}
 
 table::table(string n, string c [], bool ind [], unsigned int c_n){
 	name = n;
 	for(int i = 0; i < c_n; i++){
-		columns.push_back(c[i]);
-		indexes.push_back(ind[i]);
-	}
-	branches.push_back(branchNode(1,100,columns));
-	otools::colored_out("Created a table "+this->name+" with columns: \n", green );
-	for(int i = 0; i < c_n; i++) cout << columns[i] << (indexes[i] ? " (INDEXED) | " : " | ");
-	cout << endl;
-}
-
-branchNode::branchNode(int b, int t, vector<string> cols){
-	bottom = b;
-	top = t;
-	for(int i = 0; i < 10; i++) leaves.push_back(leafNode(cols));
-}
-
-int branchNode::getSpace(){
-	return (top - bottom) - quantity;
-}
-
-void table::insert(map<string,string> row){
-	if(branches.back().getSpace() == 0) branches.push_back(branchNode((branches.size()*100)+1, (branches.size()+1)*100, columns));
-	branches.back().insert(row);
-}
-
-int leafNode::getSpace(){
-	return data.size();
-}
-
-void branchNode::insert(map<string,string> row){
-	cout << "Inserted values" << endl << "============================================" << endl << "id: " << quantity << endl;
-	leaves[quantity/10].insert(row);
-	quantity++;
-}
-
-void leafNode::insert(map<string,string> row){
-	for(map<string, vector <string>>::iterator i = data.begin(); i != data.end(); i++){
-		cout << i->first << ": ";
-		if(row.count(i->first) == 0){
-			i->second.push_back("NULL");
-			cout << "NULL" << endl;
-		}
-		else{
-			i->second.push_back(row[i->first]);
-			cout << row[i->first] << endl;
+		columns.insert(pair<int, string>(i, c[i]));
+		if(ind[i]){
+			indexes.insert(pair<string,map<string,node*>>(c[i], map<string,node*>()));
 		}
 	}
 }
 
-leafNode::leafNode(vector<string> cols){
-	for(string col : cols){
-		data.insert(pair<string, vector <string>>(col, vector<string>()));
+void table::insert(vector<string> d){
+	node* new_node = data.push_back(d);
+	for(int i = 0; i < columns.size(); i++){
+		if(indexes.count(columns[i]) == 1){
+			indexes.at(columns[i]).insert(pair<string, node*>(d[i], new_node));
+		}
 	}
+}
+
+void table::remove(string condition){
+	int deleted = 0;
+	if(condition == "") {
+		for(int i = 0; i < columns.size(); i++){
+			if(indexes.count(columns[i]) == 1){
+				indexes.at(columns[i]).clear();
+			}
+		}
+		for(int i = data.size(); i != 0; i--){
+			data[i].remove();
+			deleted++;
+		}
+	}
+	else{
+
+		regex r("\\s+(((['\"`])[\\w\\s]*\\3)|\\w+)\\s+(=|!=|<|>|<=|>=)\\s+(((['\"`])[\\w\\s]*\\7)|\\w+)\\s+");
+		smatch m;
+		regex_search(condition, m, r);
+		if (m[0] == condition) {
+			string operand1 = m[1].str();
+			string operand2 = m[5].str();
+			string op = m[4].str();
+			int column1 = 0;
+			int column2 = 0;
+			if(operand1[1] != '"' || operand1[1] != '`' || operand1[1] != '\'')
+				for(int j = 0; j < columns.size(); j++) 
+					if(columns[j] == operand1)
+						column1 = j;
+			if(operand2[1] != '"' || operand2[1] != '`' || operand2[1] != '\'')
+				for(int j = 0; j < columns.size(); j++) 
+					if(columns[j] == operand2)
+						column2 = j;
+			if(op == "="){
+				for(int i = 0; i < data.size(); i++){
+					string op1="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op1 = operand1;
+						op1.erase(0, 1);
+						op1.erase(op1.size() - 1);
+					}
+					else{
+						op1 = data[i][column1];
+					}
+					string op2="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op2 = operand2;
+						op2.erase(0, 1);
+						op2.erase(op2.size() - 1);
+					}
+					else{
+						op2 = data[i][column2];
+					}
+					if(op1 == op2) {data[i].remove(); deleted++;}
+				}
+			}
+			else if(op == "!="){
+				for(int i = 0; i < data.size(); i++){
+					string op1="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op1 = operand1;
+						op1.erase(0, 1);
+						op1.erase(op1.size() - 1);
+					}
+					else{
+						op1 = data[i][column1];
+					}
+					string op2="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op2 = operand2;
+						op2.erase(0, 1);
+						op2.erase(op2.size() - 1);
+					}
+					else{
+						op2 = data[i][column2];
+					}
+					if(op1 != op2) {data[i].remove(); deleted++;}
+				}
+			}
+			else if(op == "<"){
+				for(int i = 0; i < data.size(); i++){
+					string op1="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op1 = operand1;
+						op1.erase(0, 1);
+						op1.erase(op1.size() - 1);
+					}
+					else{
+						op1 = data[i][column1];
+					}
+					string op2="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op2 = operand2;
+						op2.erase(0, 1);
+						op2.erase(op2.size() - 1);
+					}
+					else{
+						op2 = data[i][column2];
+					}
+					if(op1 < op2) {data[i].remove(); deleted++;}
+				}
+			}
+			else if(op == ">"){
+				for(int i = 0; i < data.size(); i++){
+					string op1="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op1 = operand1;
+						op1.erase(0, 1);
+						op1.erase(op1.size() - 1);
+					}
+					else{
+						op1 = data[i][column1];
+					}
+					string op2="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op2 = operand2;
+						op2.erase(0, 1);
+						op2.erase(op2.size() - 1);
+					}
+					else{
+						op2 = data[i][column2];
+					}
+					if(op1 > op2) {data[i].remove(); deleted++;}
+				}
+			}
+			else if(op == ">="){
+				for(int i = 0; i < data.size(); i++){
+					string op1="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op1 = operand1;
+						op1.erase(0, 1);
+						op1.erase(op1.size() - 1);
+					}
+					else{
+						op1 = data[i][column1];
+					}
+					string op2="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op2 = operand2;
+						op2.erase(0, 1);
+						op2.erase(op2.size() - 1);
+					}
+					else{
+						op2 = data[i][column2];
+					}
+					if(op1 >= op2) {data[i].remove(); deleted++;}
+				}
+			}
+			else if(op == "<="){
+				for(int i = 0; i < data.size(); i++){
+					string op1="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op1 = operand1;
+						op1.erase(0, 1);
+						op1.erase(op1.size() - 1);
+					}
+					else{
+						op1 = data[i][column1];
+					}
+					string op2="";
+					if(operand1[1] == '"' || operand1[1] == '`' || operand1[1] == '\''){
+						op2 = operand2;
+						op2.erase(0, 1);
+						op2.erase(op2.size() - 1);
+					}
+					else{
+						op2 = data[i][column2];
+					}
+					if(op1 <= op2) {data[i].remove(); deleted++;}
+				}
+			}
+		}
+		else throw DBexception("Invalid condition");
+	}
+	cout << "Deleted " << deleted << "rows from table " << name << endl;
+}
+
+void node::remove(){
+	this->prev->next = this->next;
+	this->next->prev = this->prev;
+	parent->s--;
+	delete this;
+}
+
+string node::operator[](int index){
+	return data[index];
 }
--- slabchenko_nikityuk_fe_91/back.hpp
+++ slabchenko_nikityuk_fe_91/back.hpp
@@ -14,34 +14,40 @@
 
 using namespace std;
 
-class leafNode{
-	map<string, vector <string>> data;
+class VectorList;
+
+class node{
+	VectorList* parent = nullptr; 
 public:
-	leafNode(vector<string> cols);
-	int getSpace();
-	void insert(map<string,string> row);
+	node(VectorList* p);
+	vector<string> data = vector<string>();
+	node* next = nullptr;
+	node* prev = nullptr;
+	void remove();
+	string operator[](int index);
 };
 
-class branchNode{
-	int top, bottom, quantity;
-	vector<leafNode> leaves;
+class VectorList{    
+	friend class node;
+	node* root = nullptr;
+	int s = 0;
 public:
-	branchNode(int b, int t, vector<string> cols);
-	int getSpace();
-	void insert(map<string,string> row);
+	node* push_back(vector<string> d);
+	node& operator[](int index);
+	int size();
 };
 
 class table{
-	string name="unnamed";
-	vector <string> columns;
-	vector <bool> indexes;
-	vector <branchNode> branches;
+	string name;
+	map<int, string> columns;
+	map<string, map<string, node*>> indexes; // indexes[name of the index][value of the indexed column]
+	VectorList data;
 public:
 	table(string n, string c [], bool ind [], unsigned int c_n);
-	void insert(map<string,string> row);
+	void insert(vector<string> d);
+	void remove(string condition);
+	string get_name(){return name;}
 };
-
-
 
 
 class DBexception : public exception{
